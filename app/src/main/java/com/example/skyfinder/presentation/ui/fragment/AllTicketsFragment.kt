dca5c59763ff6f3f37463931c6e6042cd69bddc4
Azamat <azamat.ahmetshin93@gmail.com>
2024-07-10 08:07:23 +0300

    all tickets layout added

---
+++ app/src/main/java/com/example/skyfinder/presentation/ui/fragment/AllTicketsFragment.kt
@@ -0,0 +1,30 @@
+package com.example.skyfinder.presentation.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.skyfinder.R
+
+
+class AllTicketsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_tickets, container, false)
+    }
+
+    companion object {
+//        fun newInstance(param1: String, param2: String) =
+//            AllTicketsFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+    }
+}
